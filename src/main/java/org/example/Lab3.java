319358b104068004f5d480b0568129cc2c7d9cd6
LLumisS <vasa16671@gmail.com>
2023-11-01 15:48:29 +0200

    cyrillic check

--- src/main/java/org/example/Lab3.java
+++ src/main/java/org/example/Lab3.java
@@ -5,24 +5,32 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Scanner;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class Lab3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] sentences;
 
         while(true) {
             try {
                 System.out.println("Enter text:");
                 StringBuffer buffer = new StringBuffer(scanner.nextLine());
+
+                if (containsCyrillic(buffer))
+                    throw new Exception();
+
                 if (buffer.length() == 0 || !buffer.toString().contains("."))
                     throw new Exception();
 
-                String[] sentences = buffer.toString().split("[.!?]+\\s*");
-                scanner.close();
+                sentences = buffer.toString().split("[.!?]+\\s*");
+
                 System.out.println("\nResult: " + Calculate(sentences) + "\n");
+                scanner.close();
                 break;
             } catch (Exception e) {
                 System.err.println("Error: Input Mismatch.\n");
-                scanner.nextLine();
             }
         }
     }
@@ -30,7 +38,7 @@ public class Lab3 {
     private static int Calculate(String[] sentences) {
         HashMap<String, Integer> wordCount = new HashMap<>();
         for (String sentence : sentences) {
-            String[] sentenceWords = sentence.split("\\s+");
+            String[] sentenceWords = sentence.split("\\P{L}+");
             ArrayList <String> allWords = new ArrayList<String>();
             for (String word : sentenceWords) {
                 word = word.toLowerCase();
@@ -51,5 +59,11 @@ public class Lab3 {
             if (element.equals(word))
                 return true;
         return false;
+    }
+
+    private static boolean containsCyrillic(StringBuffer input) {
+        Pattern pattern = Pattern.compile("[а-яА-Я]");
+        Matcher matcher = pattern.matcher(input);
+        return matcher.find();
     }
 }
